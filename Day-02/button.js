fe896ac3ba1f3d2564627d14592c07029381fd3f
Nidhish <konkanshenonikk@gmail.com>
2026-08-19 16:54:53 +0530

    Add CSS and JS files to Day-02-Nikhils contribution

---
+++ Day-02/button.js
@@ -0,0 +1,7 @@
+const togglebutton = document.getElementById("smash-button")
+const themeTitle= document.getElementById("theme-Title")
+togglebutton.addEventListener("click",()=>
+{
+    document.body.classList.toggle("dark-mode");
+    
+})
